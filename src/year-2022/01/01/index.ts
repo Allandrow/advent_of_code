d8f48a6f26ab66be196cfd51893c6623cded8252
Cyril Tiger <cyriltiger@gmail.com>
2022-12-01 18:01:35 +0100

    2022-01-01

---
+++ src/year-2022/01/01/index.ts
@@ -0,0 +1,24 @@
+import { sample } from './sample.js';
+import { input } from './input.js';
+
+const data = input || sample;
+
+const results = [] as number[];
+let total = 0;
+
+data.split('\n').forEach((line) => {
+   if (!line) {
+      results.push(total);
+      total = 0;
+   } else {
+      total += parseInt(line);
+   }
+});
+
+results.push(total);
+
+const result = Math.max(...results);
+
+console.log(result);
+
+// 69281
